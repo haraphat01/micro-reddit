53c2378ddc26115ba57725d73eb5675d19dcff4c
unknown <nusretbutunay@gmail.com>
2020-08-04 12:17:21 +0300

    linters setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-    validates :username, :email, :password, presence: true, length: { minimum: 5 }
+  validates :username, :email, :password, presence: true,
+                                          length: { minimum: 5 }
 end
